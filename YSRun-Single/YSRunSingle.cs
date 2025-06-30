5706080816c3cac05b09cd54aa60862b84123715
Andrew Plotkin <zarf@ZarfLent.local>
2025-06-30 17:07:58 -0400

    Let's get this ship on the road.

---
+++ YSRun-Single/YSRunSingle.cs
@@ -0,0 +1,13 @@
+namespace YSRunSingle
+{
+    using System;
+
+    public class YSRunSingle
+    {
+        public static int Main(string[] args)
+        {
+            Console.WriteLine("Hey.");
+            return 0;
+        }
+    }
+}
