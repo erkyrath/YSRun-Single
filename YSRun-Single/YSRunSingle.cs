51f546537aa05964585a3b2979099b5d9cba2149
Andrew Plotkin <zarf@ZarfLent.local>
2025-06-30 17:52:33 -0400

    Eh, we can parse an arg.

--- YSRun-Single/YSRunSingle.cs
+++ YSRun-Single/YSRunSingle.cs
@@ -1,3 +1,6 @@
+using System;
+using System.IO;
+
 namespace YSRunSingle
 {
     using System;
@@ -6,8 +9,27 @@ namespace YSRunSingle
     {
         public static int Main(string[] args)
         {
-            Console.WriteLine("Hey.");
+            bool start = false;
+            string gamefile = null;
+            
+            foreach (var val in args) {
+                if (val == "--start") {
+                    start = true;
+                    continue;
+                }
+                gamefile = val;
+            }
+            if (gamefile == null) {
+                Console.WriteLine("### No gamefile");
+                return 1;
+            }
+            RunTurn(gamefile, start);
             return 0;
+        }
+
+        public static void RunTurn(string gamefile, bool startgame)
+        {
+            Console.WriteLine($"### running {gamefile}, start={startgame}");
         }
     }
 }
